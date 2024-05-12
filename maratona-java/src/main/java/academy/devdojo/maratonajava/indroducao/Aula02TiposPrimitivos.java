ec6055028ffea376921e45445b8f560856f2749d
Kleber <klebernascimento@outlook.com.br>
2024-05-12 11:37:54 -0300

    Aula 11 - Tipos primitivos pt 02 - declaração e tamanho em memória

--- maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula02TiposPrimitivos.java
@@ -7,6 +7,18 @@ public class Aula02TiposPrimitivos {
          */
 
         int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 200;
+        float salarioFloat = 2500f;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean  verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+
         System.out.println("A idade é " + idade + " anos.");
+        System.out.println("Valor da variavel verdadeira BOOLEAN é " + verdadeiro);
+        System.out.println("Valor da variavel caractere CHAR é " + caractere);
     }
 }
