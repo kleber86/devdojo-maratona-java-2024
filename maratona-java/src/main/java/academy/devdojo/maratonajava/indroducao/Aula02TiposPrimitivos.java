89e57f8f715f8d174ef613f1ce0d4011c9d182f0
Kleber <klebernascimento@outlook.com.br>
2024-05-12 11:59:48 -0300

    Aula 12 - Tipos primitivos pt 03 - casting

--- maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula02TiposPrimitivos.java
@@ -7,9 +7,9 @@ public class Aula02TiposPrimitivos {
          */
 
         int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 200;
-        float salarioFloat = 2500f;
+        long numeroGrande = 100000L;
+        double salarioDouble = 200.00;
+        float salarioFloat = 2500.0F;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean  verdadeiro = true;
@@ -20,5 +20,9 @@ public class Aula02TiposPrimitivos {
         System.out.println("A idade é " + idade + " anos.");
         System.out.println("Valor da variavel verdadeira BOOLEAN é " + verdadeiro);
         System.out.println("Valor da variavel caractere CHAR é " + caractere);
+
+        // Casting
+        int cast = (int) 100000000000000L;
+        System.out.println("Retorno usando o casting " + cast);
     }
 }
