56e9058ec9c29ffaccff268d1f946b2a394bca38
Kleber <klebernascimento@outlook.com.br>
2024-05-12 16:19:23 -0300

    Aula 14 - Tipos primitivos pt 05 - Exercício

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.indroducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Fulano de Tal";
+        String endereco = "Rua De Nome Qualquer";
+        double salario = 1500.0;
+        String data = "12/05/2024";
+        String relatorio = "Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de " + salario + ", na data " + data +".";
+        System.out.println(relatorio);
+    }
+}
