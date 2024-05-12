66c80e767a1b2a744579d70860686e8079509d4a
Kleber <klebernascimento@outlook.com.br>
2024-05-12 10:53:37 -0300

    Aula 09 - Comentários

--- maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/OlaDevDojo.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/OlaDevDojo.java
@@ -1,6 +1,16 @@
 package academy.devdojo.maratonajava.indroducao;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    // Isso é um comentãrio de uma linha.
+
+    /*
+      Isso é um comenário de
+      de várias
+      linhas
+     */
     public static void main(String[] args) {
         System.out.println("pegasus sui sei ken");
     }
