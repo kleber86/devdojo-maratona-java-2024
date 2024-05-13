b80b1eb8f096c9d4228ae1b921eb1f840cb2b50f
Kleber <klebernascimento@outlook.com.br>
2024-05-12 22:25:47 -0300

    Aula 15 - Operadores pt 01 - Aritiméticos

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.indroducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+
+        System.out.println("Soma: " + n1 + n2);
+        System.out.println("Subtração: " + (n1 - n2));
+        System.out.println("Multiplicação: " + n1 * n2);
+        System.out.println("Divisão: " + n1 / n2);
+    }
+}
