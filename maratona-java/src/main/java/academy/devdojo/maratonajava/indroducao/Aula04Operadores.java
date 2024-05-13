804556dabceb33ae8d4fddfbeaab1ee89799504b
Kleber <klebernascimento@outlook.com.br>
2024-05-12 22:52:59 -0300

    Aula 16 - Operadores pt 02 - Relacionais

--- maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula04Operadores.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula04Operadores.java
@@ -9,5 +9,22 @@ public class Aula04Operadores {
         System.out.println("Subtração: " + (n1 - n2));
         System.out.println("Multiplicação: " + n1 * n2);
         System.out.println("Divisão: " + n1 / n2);
+
+        // % - Resto de divisão
+        System.out.println("Resto da divisão é: " + n1 % n2);
+        System.out.println("Resto da divisão é: " + n2 % n1);
+        System.out.println("Resto da divisão é: " + 1 % 6);
+
+        // < > >= <= == - Operadores logicos
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(n1 < n2);
+        System.out.println(n1 > n2);
+        System.out.println(n1 >= n2);
+        System.out.println(n1 <= n2);
+        System.out.println(n1 == n2);
+
     }
 }
