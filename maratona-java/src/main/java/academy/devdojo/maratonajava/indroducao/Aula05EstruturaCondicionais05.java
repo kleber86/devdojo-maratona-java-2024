49a65748570d75f741556866bd68de2ccdcd5443
Kleber <klebernascimento@outlook.com.br>
2024-05-14 21:14:05 -0300

    Aula 25 - Estruturas Condicionais pt 06 - Switch

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula05EstruturaCondicionais05.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.indroducao;
+
+public class Aula05EstruturaCondicionais05 {
+    public static void main(String[] args) {
+        byte dia = 1;
+
+        // char, int, byte, short, enum, String
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terca");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+    }
+}
