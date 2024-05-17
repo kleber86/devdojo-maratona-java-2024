3259d31d5b1462059f798e7287c6f0d17fbfb239
Kleber <klebernascimento@outlook.com.br>
2024-05-17 19:49:36 -0300

    Aula 32 - Arrays pt 01

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula07Arrays01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.indroducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[5];
+        idades[0] = 21;
+        idades[1] = 22;
+        idades[2] = 23;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+        System.out.println(idades[3]);
+    }
+}
