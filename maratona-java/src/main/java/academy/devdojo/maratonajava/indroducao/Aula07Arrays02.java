424e7ac559e65520108e97781e8dbda1911fb215
Kleber <klebernascimento@outlook.com.br>
2024-05-17 20:02:23 -0300

    Aula 33 - Arrays pt 02

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/indroducao/Aula07Arrays02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.indroducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float, double = 0
+        // char = "Espaço em branco"
+        // boolean = false
+        // String = null
+        boolean[] matriz = new boolean[10];
+        byte[] matriz2 = new byte[10];
+        char[] matriz3 = new char[10];
+        String[] matriz4 = new String[10];
+
+        System.out.println(matriz[0]);
+        System.out.println(matriz2[0]);
+        System.out.println(matriz3[0]);
+        System.out.println(matriz4[0]);
+    }
+}
