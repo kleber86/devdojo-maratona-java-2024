2cfbe60e473136fedc71ef96be73ea5e83561459
Kleber Nascimento <klebernascimento@outlook.com.br>
2024-06-06 20:05:12 -0300

    Aula 49 - Orientação Objetos - Métodos pt 06 - Parâmetros tipo referência

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        ImpressoraEstudante impressora01 = new ImpressoraEstudante();
+        Estudante estudante02 = new Estudante();
+
+        estudante01.nome = "Doko";
+        estudante01.idade = 2000;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Marin";
+        estudante02.idade = 200;
+        estudante02.sexo = 'F';
+
+        impressora01.imprime(estudante01);
+
+
+    }
+}
